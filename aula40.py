42475f5411862b320bbfa86c8ad5b719b3c7bb8c
Roberto Santos <rooh_84@hotmail.com>
2025-06-29 04:04:46 -0300

    introdução ao desempacotamento

---
+++ aula40.py
@@ -0,0 +1,8 @@
+"""
+Introdução ao desempacotamento
+"""
+_, _, nome, *resto = ['Maria', 'Helena', 'Luiz']
+print(nome)
+
+
+#PARA PEGAR UM NOME ESPECIFICO E NÃO HAVER FALHAS DE USA A FUNÇÃO *_ COMO VARIAVEL PARA (IGNORAR) AS OUTRAS
