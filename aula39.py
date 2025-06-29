4b258dc740c4bc7b215ace6a91aead8e7a4084a4
Roberto Santos <rooh_84@hotmail.com>
2025-06-29 03:52:28 -0300

    exibindo os índices na lista

---
+++ aula39.py
@@ -0,0 +1,10 @@
+lista = ['Maria', 'Helena', 'Luiz']
+lista.append('José')#ADICIONANDO MAIS UM NOME AO FINAL
+
+indices = range(len(lista))#FAZ O CICLO E CONTAGEM DE TODOS ELEMENTOS DECLARADOS
+
+
+for indice in indices:#FOR ESTA DECLARANDO A VARIAVEL INDICE PARA CADA VALOR DECLARADO (EM = IN) RANGE = INDICES
+    print(indice, lista[indice]) #Nesse laço, indice é uma variável que vai de 0 a 3.
+
+
